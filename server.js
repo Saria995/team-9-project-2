2790cd263eabbd694a5186cce92b6dacaa94d933
hutgib2 <hugowsmith13@gmail.com>
2023-05-17 17:40:30 +1000

    login routes,  model and middleware

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+const session = require('express-session');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+// Set up sessions
+const sess = {
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
 
 const sequelize = require('./config/connection');
 
@@ -16,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(require('./routes/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
